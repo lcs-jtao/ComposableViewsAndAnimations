0361973fedb7001006fb82bce943cc9a4bd2dfa3
Joyce Tao <jtao@lcs.on.ca>
2022-04-21 09:44:57 -0400

    Finished the second exercise

--- ComposableViewsAndAnimations/Views/Exercises/ExerciseTwoView.swift
+++ ComposableViewsAndAnimations/Views/Exercises/ExerciseTwoView.swift
@@ -17,6 +17,9 @@ struct ExerciseTwoView: View {
         
     // Whether to apply the animation
     @State private var useAnimation = false
+    
+    // Controls the size of the circle
+    @State private var offset: CGFloat = -150.0
 
     // MARK: Computed properties
 
@@ -27,8 +30,14 @@ struct ExerciseTwoView: View {
             VStack {
 
                 Circle()
-                    .frame(width: 200, height: 200)
+                    .frame(width: 150, height: 150)
                     .foregroundColor(.blue)
+                    .offset(x: 0, y: offset)
+                    .onTapGesture {
+                        withAnimation(.interpolatingSpring(mass: 1.0, stiffness: 1.0, damping: 0.75, initialVelocity: 10)) {
+                            offset += 40
+                        }
+                    }
                 
             }
             .navigationTitle("Exercise 2")
