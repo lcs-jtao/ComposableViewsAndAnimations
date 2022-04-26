0c96274450e6df66aee972b00dc685eae0fc027d
Joyce Tao <jtao@lcs.on.ca>
2022-04-26 18:28:55 -0400

    Improved the car model

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -19,20 +19,29 @@ struct CustomComposableView: View {
     // MARK: Computed properties
     var body: some View {
         VStack {
+            
             ZStack {
-                RoundedRectangle(cornerRadius: 25)
-                    .frame(width: 120, height: 60)
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 60, height: 50)
                     .foregroundColor(.red)
+                    .offset(x: offset - 25.0, y: 50)
                 RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 40, height: 40)
+                    .frame(width: 25, height: 30)
                     .foregroundColor(.white)
+                    .offset(x: offset - 16.0, y: 50)
             }
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 120, height: 50)
+                .foregroundColor(.red)
                 .offset(x: offset - 25.0, y: 30)
+            
             HStack {
                 ZStack {
                     Circle()
                         .frame(width: 40, height: 40)
-                    Text(":)")
+                    RoundedRectangle(cornerRadius: 3.5)
+                        .frame(width: 15, height: 15)
                         .foregroundColor(.white)
                 }
                 .rotationEffect(currentRotationAngle, anchor: .center)
@@ -40,7 +49,8 @@ struct CustomComposableView: View {
                 ZStack {
                     Circle()
                         .frame(width: 40, height: 40)
-                    Text(":)")
+                    RoundedRectangle(cornerRadius: 3.5)
+                        .frame(width: 15, height: 15)
                         .foregroundColor(.white)
                 }
                 .rotationEffect(currentRotationAngle, anchor: .center)
